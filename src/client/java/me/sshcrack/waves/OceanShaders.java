ebba541f89091d4236655091f8ea44a8f45b6abe
sshcrack <hendrik.lind777@gmail.com>
2023-08-20 21:24:03 +0200

    working on shaders, again...

--- src/client/java/me/sshcrack/waves/OceanShaders.java
+++ src/client/java/me/sshcrack/waves/OceanShaders.java
@@ -11,55 +11,48 @@ import net.coderbot.iris.shaderpack.IdMap;
 import net.coderbot.iris.shaderpack.PackDirectives;
 import net.coderbot.iris.uniforms.FrameUpdateNotifier;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Pair;
+import org.apache.commons.lang3.RegExUtils;
 import org.joml.Vector3d;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
+import static java.util.regex.Pattern.compile;
+
 public class OceanShaders {
-    private static final List<Pair<String, String>> REPLACEMENTS = Stream.of(
-            new Pair<>("gl_Vertex", "waves_vertex"),
-            new Pair<>("gl_Position", "waves_position"),
-            new Pair<>("ftransform()", "waves_position"),
-            new Pair<>("gl_Normal", "waves_normal")
+    private static final String VERTEX_SHADER = "gbuffers_waves.vsh";
+    private static final String FRAGMENT_SHADER = "gbuffers_waves.fsh";
+
+    private static Pattern variable(String variableName) {
+        return compile(String.format("%s(?![A-z])", variableName));
+    }
+
+    private static final List<Pair<Pattern, String>> REPLACEMENTS = Stream.of(
+            new Pair<>(variable("gl_Vertex"), "waves_vertex"),
+            //new Pair<>(variable("gl_Color"), "waves_color"),
+            new Pair<>(variable("gl_Position"), "waves_position"),
+            new Pair<>(compile("ftransform\\(\\)"), "waves_position"),
+            new Pair<>(variable("gl_Normal"), "waves_normal")
     ).toList();
 
 
     public static String transformVertexShader(String originalVertex) {
         var modified = originalVertex;
-        for (Pair<String, String> pair : REPLACEMENTS) {
-            modified = modified.replaceAll(pair.getLeft(), pair.getRight());
+        for (var pair : REPLACEMENTS) {
+            modified = RegExUtils.replaceAll(modified, pair.getLeft(), pair.getRight());
         }
 
-        List<String> lines;
-        try (InputStream in = WavesModClient.class.getResourceAsStream("/shader/gbuffers_waves.vsh");
-             BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-            var tempLines = new ArrayList<String>();
-            reader.lines().forEach(s -> {
-                if (s.startsWith("//"))
-                    return;
-
-                if (s.contains("//"))
-                    s = s.split("//")[0];
-
-                tempLines.add(s);
-            });
-
-            lines = tempLines;
-            // Environment is not available here, so we're just not preprocessing the source (although this might come in handy)
-            //JcppProcessor.glslPreprocessSource(lines)
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
+        List<String> lines = readShaderFile(VERTEX_SHADER);
         var nodes = groupLinesInNodes(lines);
+
         var transformer = new SingleASTTransformer<>();
         transformer.setTransformation((translationUnit, root) -> {
             translationUnit.parseAndInjectNodes(transformer, ASTInjectionPoint.BEFORE_DECLARATIONS, nodes.toArray(String[]::new));
@@ -69,6 +62,45 @@ public class OceanShaders {
 
         transformer.setPrintType(PrintType.SIMPLE);
         return transformer.transform(modified);
+    }
+
+    private static List<String> readShaderFile(String path) {
+        boolean debug = true;
+
+        if (debug)
+            MinecraftClient.getInstance().player
+                    .sendMessage(
+                            Text.literal("Warning: Debug mode is enabled.")
+                                    .setStyle(Style.EMPTY.withColor(Formatting.YELLOW))
+                    );
+
+        var readPath = String.format("/shader/%s", path);
+
+
+        try (
+                InputStream in = debug ?
+                        new FileInputStream(readPath.substring(1)) :
+                        WavesModClient.class.getResourceAsStream(readPath)
+                ;
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in))
+        ) {
+            var lines = new ArrayList<String>();
+            reader.lines().forEach(s -> {
+                if (s.startsWith("//"))
+                    return;
+
+                if (s.contains("//"))
+                    s = s.split("//")[0];
+
+                lines.add(s);
+            });
+
+            return lines;
+            // Environment is not available here, so we're just not preprocessing the source (although this might come in handy)
+            //JcppProcessor.glslPreprocessSource(lines)
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static List<String> groupLinesInNodes(List<String> lines) {
--- src/client/resources/shader/gbuffers_waves.vsh
+++ src/client/resources/shader/gbuffers_waves.vsh
@@ -34,30 +34,33 @@ const mat2 waves_octave_m = mat2(1.6, 1.2, -1.2, 1.6);
 // REQUIRED
 vec4 waves_vertex;
 vec4 waves_position;
-vec4 waves_normal;
+vec3 waves_normal;
+
+// not working on custom colors for now every shader pack just uses different implementations
+//vec4 waves_color;
 
 
-float hash(vec2 p) {
+float waves_hash(vec2 p) {
     float h = dot(p, vec2(127.1, 311.7));
     return fract(sin(h) * 43758.5453123);
 }
-float noise(in vec2 p) {
+float waves_noise(in vec2 p) {
     vec2 i = floor(p);
     vec2 f = fract(p);
     vec2 u = f * f * (3.0 - 2.0 * f);
-    return -1.0 + 2.0 * mix(mix(hash(i + vec2(0.0, 0.0)), hash(i + vec2(1.0, 0.0)), u.x), mix(hash(i + vec2(0.0, 1.0)), hash(i + vec2(1.0, 1.0)), u.x), u.y);
+    return -1.0 + 2.0 * mix(mix(waves_hash(i + vec2(0.0, 0.0)), waves_hash(i + vec2(1.0, 0.0)), u.x), mix(waves_hash(i + vec2(0.0, 1.0)), waves_hash(i + vec2(1.0, 1.0)), u.x), u.y);
 }
 
 
-float sea_octave(vec2 uv, float choppy) {
-    uv += noise(uv);
+float waves_sea_octave(vec2 uv, float choppy) {
+    uv += waves_noise(uv);
     vec2 wv = 1.0 - abs(sin(uv));
     vec2 swv = abs(cos(uv));
     wv = mix(wv, swv, wv);
     return pow(1.0 - pow(wv.x * wv.y, 0.65), choppy);
 }
 
-float map(vec3 p, int iterations) {
+float waves_map(vec3 p, int iterations) {
     float SEA_TIME = (1.0 + waves_time * waves_SEA_SPEED * 0.05);
 
     float freq = waves_SEA_FREQ;
@@ -70,8 +73,8 @@ float map(vec3 p, int iterations) {
 
     float d, h = 0.0;
     for (int i = 0; i < iterations; i++) {
-        d = sea_octave((uv + SEA_TIME) * freq, choppy);
-        d += sea_octave((uv - SEA_TIME) * freq, choppy);
+        d = waves_sea_octave((uv + SEA_TIME) * freq, choppy);
+        d += waves_sea_octave((uv - SEA_TIME) * freq, choppy);
         h += d * amp;
         uv *= waves_octave_m;
         freq *= 1.9;
@@ -82,48 +85,42 @@ float map(vec3 p, int iterations) {
 }
 
 
-float distSquared(vec2 A, vec2 B) {
-    return dot(A, B);
+float waves_squaredDistance(vec2 A, vec2 B) {
+    return pow(A.x - B.x, 2) * pow(A.y - B.y, 2);
 }
 
 // tracing
 vec3 waves_getNormal(vec3 p, float eps, int iterations) {
     vec3 n;
-    n.y = map(p, iterations);
-    n.x = map(vec3(p.x+eps,p.y,p.z), iterations) - n.y;
-    n.z = map(vec3(p.x,p.y,p.z+eps), iterations) - n.y;
+    n.y = waves_map(p, iterations);
+    n.x = waves_map(vec3(p.x+eps,p.y,p.z), iterations) - n.y;
+    n.z = waves_map(vec3(p.x,p.y,p.z+eps), iterations) - n.y;
     n.y = eps;
     return normalize(n);
 }
 
 
 void compute(vec4 vertex) {
-    float camDistSqrd = distSquared(vertex.xz, waves_cameraPosition.xz);
+    float camDistFloat = waves_squaredDistance(vertex.xz, waves_cameraPosition.xz);
 
     vec3 adjusted = waves_cameraPosition.xyz + vertex.xyz;
 
     adjusted.y = 0;
 
     int iterations = waves_MAX_ITER_GEOMETRY;
-    if(camDistSqrd > waves_FULL_RES_CHUNKS * 16)
-    iterations = waves_LOW_RES_GEOMETRY;
+    if(camDistFloat > waves_FULL_RES_CHUNKS * 16)
+        iterations = waves_LOW_RES_GEOMETRY;
 
-    float heightDiff = map(adjusted.xyz, iterations) * 5;
+    float heightDiff = waves_map(adjusted.xyz, iterations) * 5;
     vertex.y += waves_SEA_HEIGHT - heightDiff;
 
-    //vec3 color = gl_Color.xyz;
-    //color -= SEA_WATER_COLOR * (SEA_HEIGHT + heightDiff) * .2;
-    //glcolor = vec4(color, 1);
-
+    //waves_color = vec4(0,0,0,0);//gl_Color;
+    //waves_color.xyz = vec3(0, 0, 0);// -= waves_SEA_WATER_COLOR * (waves_SEA_HEIGHT + heightDiff) * .2;
 
     waves_position = gl_ProjectionMatrix * gl_ModelViewMatrix * vertex;
     waves_vertex = vertex;
 
     vec3 camDist = waves_vertex.xyz - waves_cameraPosition;
-    float RESOLUTION = .05;
-    waves_normal = vec4(
-        waves_getNormal(vertex.xyz, dot(camDist,camDist) * RESOLUTION, waves_ITER_FRAGMENT),
-
-        1
-    );
+    float RESOLUTION = .00001;
+    waves_normal.xyz = waves_getNormal(vertex.xyz, dot(camDist,camDist) * RESOLUTION, waves_ITER_FRAGMENT);
 }
